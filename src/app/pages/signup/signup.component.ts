2fb6d6ba2a8d2a76253872f1ec93dd67c169905a
Alexandre Junior <alejunior.dev@gmail.com>
2024-03-26 15:28:31 -0300

    feat: create signup page

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { InputPrimaryComponent } from '../../components/input-primary/input-primary.component';
+import { LoginService } from '../../services/login/login.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+interface SignupForm {
+  name: FormControl;
+  email: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
+}
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    InputPrimaryComponent,
+  ],
+  providers: [LoginService],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  submit() {
+    this.loginService
+      .login(this.signupForm.value.email, this.signupForm.value.password)
+      .subscribe({
+        next: () => this.toastService.success('Login feito com sucesso!'),
+        error: () =>
+          this.toastService.error(
+            'Erro inesperado! Tente novamente mais tarde'
+          ),
+      });
+  }
+
+  navigate() {
+    this.router.navigate(['login']);
+  }
+}
